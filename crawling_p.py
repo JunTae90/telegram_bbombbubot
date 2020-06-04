4532593db3b84324b616890563261d310ab1066e
JunTae90 <gustcool2@gmail.com>
2020-06-04 09:01:37 +0900

    2020.06.04

--- crawling_p.py
+++ crawling_p.py
@@ -33,30 +33,32 @@ def update_post():
     while True:
         update_keyword()
         keyword_list = list(my_dict.keys())
-        req = requests.get('http://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu')
-        html = req.text
-        soup = BeautifulSoup(html, 'html.parser')
-        posts = soup.find("tr", {"class" : "list1"})
-        current_num = posts.find("td", {"class" : "eng list_vspace"}).text
-        title = posts.find("font", {"class" : "list_title"}).text
-        if latest_num != current_num:
-            latest_num = current_num
-            for keyword in keyword_list:
-                if keyword in title:
-                    link = 'http://www.ppomppu.co.kr/zboard/'+posts.find("td", { "valign" : "middle"}).find("a").attrs['href']
-                    text_req = requests.get(link)
-                    text_html = text_req.text
-                    text_soup = BeautifulSoup(text_html, 'html.parser')
-                    text_link = text_soup.find("div", {"class": "wordfix"}).find("a").text
-                    message = '<뽐뿌 키워드 탐색> ' + keyword + '\n' + title + '\n' + link + '\n(본문 링크) ' + text_link
-                    user_list = my_dict[keyword]
-                    for user in user_list:
-                        try:
-                            bot.sendMessage(user, message)
-                        except telegram.error.Unauthorized:
-                            os.remove(os.path.join(data_dir, user))
-                            continue
-
+        try:
+            req = requests.get('http://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu')
+            html = req.text
+            soup = BeautifulSoup(html, 'html.parser')
+            posts = soup.find("tr", {"class" : "list1"})
+            current_num = posts.find("td", {"class" : "eng list_vspace"}).text
+            title = posts.find("font", {"class" : "list_title"}).text
+            if latest_num != current_num:
+                latest_num = current_num
+                for keyword in keyword_list:
+                    if keyword in title:
+                        link = 'http://www.ppomppu.co.kr/zboard/'+posts.find("td", { "valign" : "middle"}).find("a").attrs['href']
+                        text_req = requests.get(link)
+                        text_html = text_req.text
+                        text_soup = BeautifulSoup(text_html, 'html.parser')
+                        text_link = text_soup.find("div", {"class": "wordfix"}).find("a").text
+                        message = '<뽐뿌 키워드 탐색> ' + keyword + '\n' + title + '\n' + link + '\n(본문 링크) ' + text_link
+                        user_list = my_dict[keyword]
+                        for user in user_list:
+                            try:
+                                bot.sendMessage(user, message)
+                            except telegram.error.Unauthorized:
+                                os.remove(os.path.join(data_dir, user))
+                                continue
+        except:
+            continue
         time.sleep(0.5)
 
 def crawling_p():
